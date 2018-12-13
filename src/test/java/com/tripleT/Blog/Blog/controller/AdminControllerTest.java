b84a03b627bc4237bc5c33ae5df266ef3f5e1e18
tuan11997 <201584000@student.hust.edu.vn>
2018-12-13 15:39:19 +0700

    Add controller test Add Blog

---
+++ src/test/java/com/tripleT/Blog/Blog/controller/AdminControllerTest.java
@@ -0,0 +1,41 @@
+package com.tripleT.Blog.Blog.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AdminControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void showCreateForm() {
+    }
+
+    @Test
+    public void saveUser() {
+    }
+
+    @Test
+    public void listUser() {
+    }
+
+    @Test
+    public void showEditForm() {
+    }
+
+    @Test
+    public void updateUser() {
+    }
+
+    @Test
+    public void showDeleteForm() {
+    }
+
+    @Test
+    public void deleteUser() {
+    }
+}
