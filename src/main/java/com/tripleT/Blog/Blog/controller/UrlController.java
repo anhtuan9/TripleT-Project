6e83751866a03edf667cc832672c2c8186df7d4f
anhtuan9 <batmanlate@gmail.com>
2018-12-15 00:55:30 +0700

    1/2

--- src/main/java/com/tripleT/Blog/Blog/controller/UrlController.java
+++ src/main/java/com/tripleT/Blog/Blog/controller/UrlController.java
@@ -1,10 +1,16 @@
 package com.tripleT.Blog.Blog.controller;
 
+import com.tripleT.Blog.Blog.model.Blog;
+import com.tripleT.Blog.Blog.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,15 +18,18 @@ import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class UrlController {
+    @Autowired
+    private BlogService blogService;
     @GetMapping("/")
-    public String index() {
-        return "index1";
+    public ModelAndView index(Pageable pageable) {
+        ModelAndView modelAndView = new ModelAndView("index1");
+        Page<Blog> blogs;
+        blogs = blogService.findAll(pageable);
+        modelAndView.addObject("blogs", blogs);
+        return modelAndView;
+
     }
 
-    @GetMapping("/admin")
-    public String admin() {
-        return "admin";
-    }
 
     @GetMapping("/403")
     public String accessDenied() {
